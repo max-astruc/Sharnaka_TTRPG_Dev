888147fb97a91e4c6b8b7f0b761c7a7f00a460c7
max-astruc <maxence.astruc@lyceeastier.com>
2026-05-09 23:55:29 +0200

    Ajoutez des fichiers projet.

---
+++ Sharnaka_Dev/TTRPG_Structure/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharnaka_Dev.TTRPG_Structure
+{
+    internal class Character
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+
+        public Species species { get; set; }
+        public Race race {  get; set; }
+
+        public bool IsAlive { get; set; }
+    }
+}
---
+++ Sharnaka_Dev/TTRPG_Structure/Species.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharnaka_Dev.TTRPG_Structure
+{
+    enum Species
+    {
+       Artilects,
+       Astrel,
+       Cycléides,
+       Joricien,
+       Murci,
+       Tiefflin,
+    }
+
+    enum Races
+    {
+        //Races Tiefflin :
+        Tiefflin_Aeris,
+        Tiefflin_Akwo,
+        Tiefflin_Pyris,
+        Tiefflin_Litho,
+
+        //Races Murci :
+        Murci_Hipsy,
+        Murci_Lavia,
+    }
+}
